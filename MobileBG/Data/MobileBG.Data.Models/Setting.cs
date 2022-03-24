debe16f8ca709b123cad2c853074f9add7779804
Simeon Petkov <simeonpetkov1999@gmail.com>
2022-03-24 11:20:04 +0200

    Initial Create

---
+++ MobileBG/Data/MobileBG.Data.Models/Setting.cs
@@ -0,0 +1,11 @@
+﻿namespace MobileBG.Data.Models
+{
+    using MobileBG.Data.Common.Models;
+
+    public class Setting : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ MobileBG/MobileBG.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace MobileBG.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "MobileBG";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
